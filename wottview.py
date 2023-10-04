26e4169553383aa1c097794e5631f9b3835699d2
ddomonoske <domonoske@higherground.earth>
2023-10-03 17:54:17 -0700

    Added button handlers, controller interface, and pseudocode for the major view updates

--- wottview.py
+++ wottview.py
@@ -5,31 +5,38 @@ class View(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.controller = None
+
         # set up grid scaling
         self.columnconfigure((0,1), weight=0)
         self.columnconfigure(2, weight=1)
+        self.columnconfigure(2, minsize=200)
         self.rowconfigure(0,weight=1)
 
         # top selection frame
-        self.topSelect_frm = ctk.CTkFrame(self, width=140, corner_radius=0)
-        
+        self.topSelect_frm = ctk.CTkFrame(self, width=140, corner_radius=0, fg_color=("gray60", "gray10"))
+
         # logo
         self.logo_lbl = ctk.CTkLabel(self.topSelect_frm, text="WOTTProject", font=ctk.CTkFont(size=20, weight="bold"))
         self.logo_lbl.grid(row=0, column=0, padx=20, pady=(20, 10))
-        
+
         # top selection buttons
-        self.riders_btn = ctk.CTkButton(master=self.topSelect_frm, text="Rider Profiles")
-        self.riders_btn.grid(row=1, column=0, pady=5)
-        self.envir_btn = ctk.CTkButton(master=self.topSelect_frm, text="Environments")
+        self.rider_btn = ctk.CTkButton(master=self.topSelect_frm, text="Rider Profiles", command=self.riderBtnPress)
+        self.rider_btn.grid(row=1, column=0, pady=5)
+        self.envir_btn = ctk.CTkButton(master=self.topSelect_frm, text="Environments", command=self.envirBtnPress)
         self.envir_btn.grid(row=2, column=0, pady=5)
-        self.sims_btn = ctk.CTkButton(master=self.topSelect_frm, text="Simulations")
-        self.sims_btn.grid(row=3, column=0, pady=5)
-        
+        self.sim_btn = ctk.CTkButton(master=self.topSelect_frm, text="Simulations", command=self.simBtnPress)
+        self.sim_btn.grid(row=3, column=0, pady=5)
+
         self.topSelect_frm.grid(row=0, column=0, padx=0, pady=0, sticky="news")
 
-        # sub selection frame
+        # sub selection frame, empty at first
+        self.subSelect_frm = ctk.CTkFrame(self, corner_radius=0)
+        self.subSelect_frm.grid(row=0, column=1, padx=0, pady=0, sticky="news")
 
-        # main content frame
+        # main content frame, empty at first
+        self.mainContent_frm = ctk.CTkFrame(self, corner_radius=0)
+        self.mainContent_frm.grid(row=0, column=2, padx=0, pady=0, sticky="news")
 
         # rider frame
         self.rider_frm = RiderProfilesFrame(self)
@@ -40,6 +47,52 @@ class View(ctk.CTkFrame):
         # create simulations frame
         self.sim_frm = SimulationProfilesFrame(self)
 
+    ### ------ connect controller ------ ###
+    def setController(self, controller):
+        self.controller = controller
+
+    ### ------ button handlers ------ ###
+    def riderBtnPress(self):
+        if self.controller:
+            self.controller.riderBtnPress()
+
+    def envirBtnPress(self):
+        if self.controller:
+            self.controller.envirBtnPress()
+
+    def simBtnPress(self):
+        if self.controller:
+            self.controller.simBtnPress()
+
+    ### ------ update view methods ------ ###
+
+    # to update entire view when top level buttons are clicked
+    def showRiders(riderList: list):
+        # show list of riders in sub selection frame
+        rider = riderList[1]
+        # clear main content frame
+
+    def showEnvironments(envirList: list):
+        # show list of environments in sub selection frame
+        envir = envirList[1]
+        # clear main content frame
+
+    def showSimulations(simList: list[str]):
+        # show list of simulations in sub selection frame
+        sim = simList[1]
+        # clear main content frame
+
+    def showRiderDetail(rider):
+        # show rider details in main content frame
+        rider
+
+    def showEnvirDetail(envir):
+        # show environment details in main content frame
+        envir
+
+    def showSimDetail(sim):
+        # show sim details in main content frame
+        sim
 
 # Rider Profiles main content frame
 class RiderProfilesFrame(ctk.CTkFrame):
@@ -58,10 +111,13 @@ class SimulationProfilesFrame(ctk.CTkFrame):
 
 # for testing
 if __name__ == '__main__':
+    ctk.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+    ctk.set_default_color_theme("blue")  # ["blue", "green", "dark-blue", "sweetkind"]
+
     app = ctk.CTk()
     app.title("WOTT Main View Demo")
     app.state("zoomed")
-    
+
     app.columnconfigure(0,weight=1)
     app.rowconfigure(0,weight=1)
 
