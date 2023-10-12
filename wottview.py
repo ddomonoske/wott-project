a300c83cc8445502ed5763222532fddb4485934d
ddomonoske <domonoske@higherground.earth>
2023-10-11 21:35:31 -0600

    adding `grid_` to all column and row configures

--- wottview.py
+++ wottview.py
@@ -10,13 +10,15 @@ class View(ctk.CTkFrame):
         self.controller = None
 
         # set up grid scaling
-        self.columnconfigure((0,1), weight=0)
-        self.columnconfigure(2, weight=1)
-        self.columnconfigure(2, minsize=200)
-        self.rowconfigure(0,weight=1)
+        self.grid_columnconfigure((0,1), weight=0)
+        self.grid_columnconfigure(0, minsize=200)
+        self.grid_columnconfigure(1, minsize=150)
+        self.grid_columnconfigure(2, weight=1, minsize=200)
+        self.grid_rowconfigure(0,weight=1)
 
         # top selection frame
-        self.topSelect_frm = ctk.CTkFrame(self, width=140, corner_radius=0, fg_color=("gray60", "gray10"))
+        self.topSelect_frm = ctk.CTkFrame(self, corner_radius=0, fg_color=("gray60", "black"))
+        self.topSelect_frm.grid_columnconfigure(0, weight=1)
 
         # logo
         self.logo_lbl = ctk.CTkLabel(self.topSelect_frm, text="WOTTProject", font=ctk.CTkFont(size=20, weight="bold"))
@@ -79,7 +81,7 @@ class View(ctk.CTkFrame):
     def updateSubSelectionFrame(self, list: List[str]):
         # show list of riders in sub selection frame
         self.subSelect_frm = SubSelectFrame(self, list, self.controller)
-        self.subSelect_frm.grid(row=0, column=1, padx=0, pady=0, sticky="news")
+        self.subSelect_frm.grid(row=0, column=1, padx=0, pady=0, sticky="NSEW")
         # TODO clear main content frame
 
     def showRiderDetail(rider):
@@ -98,23 +100,27 @@ class View(ctk.CTkFrame):
 # General scrollable frame for riders, environments, or simulations
 class SubSelectFrame(ctk.CTkScrollableFrame):
     def __init__(self, parent, names: List[str], controller=None):
-        super().__init__(parent, fg_color=("gray70", "gray10"))
+        super().__init__(parent, fg_color=("gray70", "gray10"), width=1, corner_radius=0)
 
         self.controller = controller
+
+        self.grid_columnconfigure(0, weight=1)
 
         # list of btns for each name
         self.name_btns = []
 
         # Add button
-        self.add_btn = ctk.CTkButton(self, text="Add New", fg_color="green")
+        self.add_btn = ctk.CTkButton(self, text="Add New", fg_color="green", hover_color="dark green", width=50)
         self.add_btn.grid(row=0, column=0, pady=(30,10))
+        
+        bg_color = self.cget("fg_color")
 
         # Create a btn for each name and append to list
         for i, name in enumerate(names, start=1):
-            btn = ctk.CTkButton(self, text=name, corner_radius=0, border_width=1, border_color="black",
+            btn = ctk.CTkButton(self, text=name, corner_radius=0, border_width=1, border_color=bg_color,
                                 command=partial(self.subSelectBtnPress, name))
             self.name_btns.append(btn)
-            btn.grid(row=i, column=0)
+            btn.grid(row=i, column=0, sticky="EW")
 
     """ ------ connect controller ------ """
     def setController(self, controller):
