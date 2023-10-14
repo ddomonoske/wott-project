67064dc7b386929f7d6ec1585bbb2afcee2218e0
ddomonoske <domonoske@higherground.earth>
2023-10-13 20:33:33 -0600

    Add a test for `loadObject()` and `saveObject()`. Also beautified the test output

--- test_wottmodel.py
+++ test_wottmodel.py
@@ -1,48 +1,131 @@
 from wottmodel import *
+from pathlib import Path
 
-# TODO make these do more then the absolute barebones call
+# colors from blender
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    EMPH = BOLD+UNDERLINE
 
-def test_loadModel():
-    print("Test loadModel")
-    model = Model()
-    model.loadModel()
+def printSuccess(text: str = "success"):
+    print(f"{bcolors.OKGREEN}{text}{bcolors.ENDC}")
 
-def test_loadRiders():
-    print("Test loadRiders")
-    model = Model()
-    model.loadRiders()
+def printFailure(text: str = "failure"):
+    print(f"{bcolors.FAIL}{text}{bcolors.ENDC}")
 
-def test_loadEnvirs():
-    print("Test loadEnvirs")
-    model = Model()
-    model.loadEnvirs()
+def test_saveObject_loadObject() -> int:
+    # instanciate model with a test directory
+    testDirectory = Path.cwd() / "wott_test_saveObject_loadObject"
+    model = Model(str(testDirectory))
 
-def test_loadSims():
-    print("Test loadSims")
-    model = Model()
-    model.loadSims()
+    ridersFile = testDirectory / "ridersFile"
+    testObject = ["this","is","my","data"]
+    model.saveObject(filePath=ridersFile, obj=testObject)
 
-def test_saveModel():
-    print("Test saveModel")
-    model = Model()
-    model.saveModel()
+    returnObject = model.loadObject(ridersFile)
 
-def test_saveRiders():
-    print("Test saveRiders")
-    model = Model()
-    model.saveRiders()
+    # delete file and directory
+    ridersFile.unlink()
+    testDirectory.rmdir()
 
-def test_saveEnvirs():
-    print("Test saveEnvirs")
-    model = Model()
-    model.saveEnvirs()
+    if (returnObject == testObject):
+        printSuccess("test_saveObject_loadObject")
+        return 0
+    else:
+        printFailure("test_saveObject_loadObject")
+        return 1
 
-def test_saveSims():
-    print("Test saveSims")
-    model = Model()
-    model.saveSims()
+# TODO make these do more then the absolute barebones calls 
+def test_loadRiders() -> int:
+    try:
+        model = Model()
+        model.loadRiders()
+        printSuccess("test_loadRiders")
+        return 0
+    except:
+        printFailure("test_loadRiders")
+        return 1
 
-test_list = [test_loadRiders,
+def test_loadEnvirs() -> int:
+    try:
+        model = Model()
+        model.loadEnvirs()
+        printSuccess("test_loadEnvirs")
+        return 0
+    except:
+        printFailure("test_loadEnvirs")
+        return 1
+
+def test_loadSims() -> int:
+    try:
+        model = Model()
+        model.loadSims()
+        printSuccess("test_loadSims")
+        return 0
+    except:
+        printFailure("test_loadSims")
+        return 1
+
+def test_loadModel() -> int:
+    try:
+        model = Model()
+        model.loadModel()
+        printSuccess("test_loadModel")
+        return 0
+    except:
+        printFailure("test_loadModel")
+        return 1
+
+def test_saveModel() -> int:
+    try:
+        model = Model()
+        model.saveModel()
+        printSuccess("test_saveModel")
+        return 0
+    except:
+        printFailure("test_saveModel")
+        return 1
+
+def test_saveRiders() -> int:
+    try:
+        model = Model()
+        model.saveRiders()
+        printSuccess("test_saveRiders")
+        return 0
+    except:
+        printFailure("test_saveRiders")
+        return 1
+
+def test_saveEnvirs() -> int:
+    try:
+        model = Model()
+        model.saveEnvirs()
+        printSuccess("test_saveEnvirs")
+        return 0
+    except:
+        printFailure("test_saveEnvirs")
+        return 1
+
+def test_saveSims() -> int:
+    try:
+        model = Model()
+        model.saveSims()
+        printSuccess("test_saveSims")
+        return 0
+    except:
+        printFailure("test_saveSims")
+        return 1
+
+
+test_list = [test_saveObject_loadObject,
+             test_loadRiders,
              test_loadEnvirs,
              test_loadSims,
              test_loadModel,
@@ -53,5 +136,13 @@ test_list = [test_loadRiders,
 
 # run all tests
 if __name__ == '__main__':
+    failedTests = 0
     for test in test_list:
-        test()
+        if test():
+            failedTests += 1
+
+    if failedTests:
+        printFailure(f"{bcolors.EMPH}\n{failedTests} test(s) failed")
+        printSuccess(f"{bcolors.EMPH}{len(test_list)-failedTests} test(s) passed")
+    else:
+        printSuccess(f"{bcolors.EMPH}\nAll {len(test_list)} tests passed")
