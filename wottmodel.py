963b88866135847eb2bf4fb1ff3901e7aefeab3f
ddomonoske <domonoske@higherground.earth>
2023-10-18 20:46:07 -0400

    added `Model.getRider()` method

--- wottmodel.py
+++ wottmodel.py
@@ -146,7 +146,15 @@ class Model(object):
         pickle.dump(obj, f)
 
 
-    # get specific rider, given a string
+    # get specific rider, given a riderID
+    # TODO why can't I write that this returns Rider without an error?
+    # TODO ie def getRider(self, riderID: int) -> Rider:
+    def getRider(self, riderID: int):
+        for rider in self.riders:
+            if rider.isRider():
+                return rider
+        return None
+
     # get specific environment, given a string
     # get specific simulation, given a string
 
@@ -227,6 +235,9 @@ class Rider(object):
 
     def getNameID(self) -> tuple[str,int]:
         return (self.getName, self.getID)
+
+    def isRider(self, id: int):
+        return self.riderID == id
 
 class Environment(object):
     def __init__(self) -> None:
