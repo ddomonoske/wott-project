83cc3d6ab77f9680a8d24a5abc450344afbb1638
ddomonoske <domonoske@higherground.earth>
2023-10-23 19:36:21 -0300

    new test for `Controller.replaceEmptyName()` and framework for running non-GUI tests

--- test_wottcontroller.py
+++ test_wottcontroller.py
@@ -2,9 +2,39 @@ import customtkinter as ctk
 from wottmodel import *
 from wottview import *
 from wottcontroller import *
+from test_helpers import *
+
+def test_Controller_replaceEmptyName() -> int:
+    try:
+        newStrs = Controller.replaceEmptyName(riderStr)
+        for nameID in newStrs:
+            if nameID[0] == " ":
+                raise Exception
+        printSuccess("test_Controller_replaceEmptyName")
+        return 0
+    except:
+        printFailure("test_Controller_replaceEmptyName")
+        return 1
+
+test_list = [
+    test_Controller_replaceEmptyName
+]
 
 # run all tests
 if __name__ == '__main__':
+    # run the non-GUI tests
+    failedTests = 0
+    for test in test_list:
+        if test():
+            failedTests += 1
+
+    if failedTests:
+        printFailure(f"{bcolors.EMPH}\n{failedTests} test(s) failed")
+        printSuccess(f"{bcolors.EMPH}{len(test_list)-failedTests} test(s) passed")
+    else:
+        printSuccess(f"{bcolors.EMPH}\nAll {len(test_list)} tests passed")
+
+    # run the GUI test
     ctk.set_default_color_theme("blue")
     app = ctk.CTk()
     app.wm_state("zoomed")
