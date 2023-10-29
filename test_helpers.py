4dabee27e766c8929e9f6d5f937f4947e332e811
ddomonoske <domonoske@higherground.earth>
2023-10-29 08:26:01 -0300

    Moved `BasicController` to `test_helpers.py`

--- test_helpers.py
+++ test_helpers.py
@@ -1,3 +1,8 @@
+import customtkinter as ctk
+from typing import Dict
+from wottview import *
+from functools import partial
+
 fruitStr = [("apple", 1),
             ("banana", 2),
             ("cherry", 5),
@@ -36,8 +41,53 @@ class bcolors:
     UNDERLINE = '\033[4m'
     EMPH = BOLD+UNDERLINE
 
+class BasicController():
+    def __init__(self, view: ctk.CTkFrame = None) -> None:
+        self.view = view
+
+    def addRiderBtnPress(self):
+        print("add rider button pressed")
+
+    def addEnvirBtnPress(self):
+        print("add environment button pressed")
+
+    def addSimBtnPress(self):
+        print("add simulation button pressed")
+
+    def riderSelectBtnPress(self, id: int):
+        print(f"rider {id} button pressed")
+
+    def envirSelectBtnPress(self, id: int):
+        print(f"environment {id} button pressed")
+
+    def simSelectBtnPress(self, id: int):
+        print(f"simulation {id} button pressed")
+
+    def riderBtnPress(self):
+        if self.view:
+            self.view.showRiderSelectionList(riderStr)
+
+    def envirBtnPress(self):
+        if self.view:
+            self.view.showEnvirSelectionList(envirStr)
+
+    def simBtnPress(self):
+        if self.view:
+            self.view.showSimSelectionList(simStr)
+
+    def saveRiderBtnPress(self, riderID=-1, attributeDict: Dict[str, str] = {}):
+        print(f"rider {riderID} updated:" + str(attributeDict))
+
 def printSuccess(text: str = "success"):
     print(f"{bcolors.OKGREEN}{text}{bcolors.ENDC}")
 
 def printFailure(text: str = "failure"):
     print(f"{bcolors.FAIL}{text}{bcolors.ENDC}")
+
+def repeatSuccessAlert(rp_frame: RiderProfileFrame):
+    rp_frame.showAlertSuccess("Success Alert Message", 1000)
+    rp_frame.after(2000, partial(repeatErrorAlert, rp_frame))
+
+def repeatErrorAlert(rp_frame: RiderProfileFrame):
+    rp_frame.showAlertError("Error Alert Message", 1000)
+    rp_frame.after(2000, partial(repeatSuccessAlert, rp_frame))
