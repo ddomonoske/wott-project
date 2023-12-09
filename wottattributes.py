87cd4734b9229cc8e151fb3543f562d91839c81e
ddomonoske <domonoske@higherground.earth>
2023-12-08 20:29:05 -0700

    New sets for attributes Implemented as `@classmethod` Now an attribute can be chacked against all valid ones

--- wottattributes.py
+++ wottattributes.py
@@ -13,12 +13,31 @@ class RiderAttributes(object):
     CDA = "CdA"
     POWERRESULTS = "powerResults"
 
+    @classmethod
+    def set(cls):
+        return {cls.RIDERID,
+                cls.FIRSTNAME,
+                cls.LASTNAME,
+                cls.WEIGHT,
+                cls.FTP,
+                cls.WPRIME,
+                cls.CDA,
+                cls.POWERRESULTS}
+
 class EnvirAttributes(object):
     ENVIRID = "envirID"
     ENVIRNAME = "envirName"
     AIRDENSITY = "airDensity"
     CRR = "Crr"
     MECHLOSSES = "mechLosses"
+
+    @classmethod
+    def set(cls):
+        return {cls.ENVIRID,
+                 cls.ENVIRNAME,
+                cls.AIRDENSITY,
+                cls.CRR,
+                cls.MECHLOSSES}
 
 class SimAttributes(object):
     SIMID = "simID"
@@ -29,6 +48,17 @@ class SimAttributes(object):
     RIDERLIST = "riderList"
     ENVIRLIST = "envirList"
     POWERPLAN = "powerPlan"
+
+    @classmethod
+    def set(cls):
+        return {cls.SIMID,
+                cls.SIMNAME,
+                cls.RIDER,
+                cls.ENVIR,
+                cls.MODEL,
+                cls.RIDERLIST,
+                cls.ENVIRLIST,
+                cls.POWERPLAN}
 
 # these rely on the above constants to ensure compatibility
 class IPCalcAttributes(object):
@@ -42,3 +72,16 @@ class IPCalcAttributes(object):
     RACEDISTANCE = "raceDistance"
     DT = "dt"
     V0 = "v0"
+
+    @classmethod
+    def set(cls):
+        return {cls.CDA,
+                cls.AIRDENSITY,
+                cls.MASSKG,
+                cls.CRR,
+                cls.MECHLOSSES,
+                cls.POWERPLAN,
+                cls.MAXFORCE,
+                cls.RACEDISTANCE,
+                cls.DT,
+                cls.V0}
